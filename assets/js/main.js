513e0ce7add81016a63d15d68701124eb21a62a9
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2023-11-15 17:27:40 +0100

    login & home form

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+ function toggleDropdown() {
+    var dropdownContainer = document.querySelector(".dropdown-container");
+    dropdownContainer.style.display = (dropdownContainer.style.display === "block") ? "none" : "block";
+}
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>Document</title>
+</head>
+
+<body class="body-home">
+    <nav>
+        <img src="assets/imgs/logo.png" alt="logo" height="60" width="300">
+        <div>
+            <p>+212601020304</p>
+            <p>electro_nacer@gmail.com</p>
+        </div>
+    </nav>
+    <section>
+        <div class="sidebar">
+            <p class="active">Tous les produits</p>
+
+            <button class="dropdown-btn" onclick="toggleDropdown()">Catégories de produits
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-container">
+                <p>Categorie 1</p>
+                <p>Categorie 2</p>
+                <p>Categorie 3</p>
+            </div>
+            <p>Les produits en rupture de stock</p>
+
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
