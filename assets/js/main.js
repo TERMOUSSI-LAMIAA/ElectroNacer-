20b10359161381ebac154865fd9f3167646649ec
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2023-12-03 22:45:31 +0100

    add bootstrap

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,140 @@
-function filterProducts() {
+// function filterProducts() {
     
-    var selectedCategory = document.getElementById("categories").value;
-    var products = document.getElementsByClassName("product");
+//     var selectedCategory = document.getElementById("categories").value;
+//     var products = document.getElementsByClassName("product");
 
-    for (var i = 0; i < products.length; i++) {
-        products[i].style.display = "none";
+//     for (var i = 0; i < products.length; i++) {
+//         products[i].style.display = "none";
+//     }
+
+//     if (selectedCategory === "tout") {
+//         for (var i = 0; i < products.length; i++) {
+//             products[i].style.display = "block";
+//         }
+//     } else {
+//         var selectedCategoryProducts = document.getElementsByClassName("category-" + selectedCategory);
+//         for (var i = 0; i < selectedCategoryProducts.length; i++) {
+//             selectedCategoryProducts[i].style.display = "block";
+//         }
+//     }
+// }
+/**
+ * Created by Kupletsky Sergey on 17.10.14.
+ *
+ * Material Sidebar (Profile menu)
+ * Tested on Win8.1 with browsers: Chrome 37, Firefox 32, Opera 25, IE 11, Safari 5.1.7
+ * You can use this sidebar in Bootstrap (v3) projects. HTML-markup like Navbar bootstrap component will make your work easier.
+ * Dropdown menu and sidebar toggle button works with JQuery and Bootstrap.min.js
+ */
+
+// Sidebar toggle
+//
+// -------------------
+$(document).ready(function() {
+    var overlay = $('.sidebar-overlay');
+
+    $('.sidebar-toggle').on('click', function() {
+        var sidebar = $('#sidebar');
+        sidebar.toggleClass('open');
+        if ((sidebar.hasClass('sidebar-fixed-left') || sidebar.hasClass('sidebar-fixed-right')) && sidebar.hasClass('open')) {
+            overlay.addClass('active');
+        } else {
+            overlay.removeClass('active');
+        }
+    });
+
+    overlay.on('click', function() {
+        $(this).removeClass('active');
+        $('#sidebar').removeClass('open');
+    });
+
+});
+
+// Sidebar constructor
+//
+function htmlbodyHeightUpdate() {
+    var height3 = $(window).height();
+    var height1 = $('.nav').height() + 50;
+    height2 = $('.container-main').height();
+    if (height2 > height3) {
+        $('html').height(Math.max(height1, height3, height2) + 10);
+        $('body').height(Math.max(height1, height3, height2) + 10);
+    } else
+    {
+        $('html').height(Math.max(height1, height3, height2));
+        $('body').height(Math.max(height1, height3, height2));
     }
 
-    if (selectedCategory === "tout") {
-        for (var i = 0; i < products.length; i++) {
-            products[i].style.display = "block";
-        }
-    } else {
-        var selectedCategoryProducts = document.getElementsByClassName("category-" + selectedCategory);
-        for (var i = 0; i < selectedCategoryProducts.length; i++) {
-            selectedCategoryProducts[i].style.display = "block";
-        }
-    }
 }
+$(document).ready(function () {
+    htmlbodyHeightUpdate();
+    $(window).resize(function () {
+        htmlbodyHeightUpdate();
+    });
+    $(window).scroll(function () {
+        height2 = $('.container-main').height();
+        htmlbodyHeightUpdate();
+    });
+});
+/*database*/
+// document.addEventListener('DOMContentLoaded', function () {
+//     var mainContent = document.getElementById('main-content');
+
+//     // Add a click event listener to the link
+//     document.querySelector('.nav-link.active').addEventListener('click', function (event) {
+//         // Prevent the default link behavior
+//         event.preventDefault();
+
+//         // Load the content of all_products.php into the main content area
+//         fetch('?view=all_products')
+//             .then(response => response.text())
+//             .then(data => {
+//                 mainContent.innerHTML = data;
+//             })
+//             .catch(error => {
+//                 console.error('Error fetching data:', error);
+//             });
+//     });
+// });
+document.addEventListener('DOMContentLoaded', function () {
+    var mainContent = document.getElementById('main-content');
+    var navLinks = document.querySelectorAll('.nav-link');
+
+    // Function to load content based on the view
+    function loadContent(view) {
+        fetch('?view=' + view)
+            .then(response => response.text())
+            .then(data => {
+                mainContent.innerHTML = data;
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }
+
+    // Add a click event listener to each navigation link
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            // Prevent the default link behavior
+            event.preventDefault();
+
+            // Remove the 'active' class from all links
+            navLinks.forEach(function (navLink) {
+                navLink.classList.remove('active');
+            });
+
+            // Add the 'active' class to the clicked link
+            link.classList.add('active');
+
+            // Determine the view based on the clicked link
+            var view = link.getAttribute('data-view');
+
+            // Load the content based on the view
+            loadContent(view);
+        });
+    });
+
+    // Load all products on page loading
+    loadContent('all_products');
+});
+
--- index.php
+++ index.php
@@ -2,143 +2,161 @@
 session_start();
 include 'db.php';
 
+// Handle different views based on the 'view' parameter
+if (isset($_GET['view'])) {
+    $view = $_GET['view'];
 
+    switch ($view) {
+        case 'all_products':
+            // Fetch all products from the database
+            $sql = "SELECT * FROM produit";
+            break;
+        case 'out_of_stock':
+            // Fetch products with qte_stock less than 3
+            $sql = "SELECT * FROM produit WHERE qte_stock <= qte_min";
+            break;
+        default:
+            // Fetch products based on the selected category
+            $category = $pdo->quote($view);
+            $sql = "SELECT * FROM produit WHERE fk_idCat = (SELECT idCat FROM categorie WHERE nomCat = $category)";
+            break;
+    }
 
+    $result = $pdo->query($sql);
+
+    // Display the products
+    if ($result->rowCount() > 0) {
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            echo '<div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="data:image/jpeg;base64,' . base64_encode($row["image"]) . '" class="card-img-top" alt="image">      
+                        <div class="card-body">
+                            <h5 class="card-title">' . $row["libelle"] . '</h5>
+                            <p class="card-text">Ref prod: ' . $row["ref_prod"] . '</p>
+                            <p class="card-text">PRIX UNITAIRE: ' . $row["pru"] . ' DH</p>
+                            <p class="card-text">qte stock: ' . $row["qte_stock"] . '</p>
+                        </div>
+                    </div>
+                </div>';
+        }
+    } else {
+        echo '<div class="alert alert-info" role="alert" style=margin:60px ;>
+        <strong>No electronic products found!</strong> 
+      </div>';
+    }
+    // Stop further execution to prevent rendering the rest of the page
+    exit();
+}
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap Dashboard</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Custom CSS -->
     <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Document</title>
 </head>
 
-<body class="body-home">
-    <nav>
-        <div class="logo">
-            <img src="assets/imgs/logo1.png" alt="logo" height="60" width="60">
-            <p>ElectroNacer</p>
-        </div>
+<body>
 
-        <div>
-            <p>+212601020304</p>
-            <p>electro_nacer@gmail.com</p>
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <!-- Logo -->
+        <a class="navbar-brand" href="index.php">
+            <img src="assets/imgs/logo1.png" alt="Logo" height="40">
+            ElectroNacer
+        </a>
+        <!-- Toggle Button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Navigation Links -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Logout</a>
+                </li>
+            </ul>
         </div>
     </nav>
-    <section class="wrapper">
-        <div class="sidebar">
 
-            <select id="categories" name="categories" onchange="filterProducts()">
-                <option value="tout">Tout les produits</option>
-                <option value="1">cartes de developpement</option>
-                <option value="2">capteurs</option>
-                <option value="3">composants electroniques</option>
-            </select>
 
-            <a href="?showOutOfStock=true">Les produits en rupture de stock</a>
-            <a href="logout.php">Déconnexion</a>
-        </div>
+    <div class="container-fluid">
+        <div class="row">
 
-        <div class="main-content" id="productList">
-            <?php
-            $query = "SELECT * FROM produit";
-            $statement = $pdo->prepare($query);
-            $statement->execute();
-            $result = $statement->fetchAll();
-
-            $showOutOfStock = isset($_GET['showOutOfStock']);
-
-            if ($showOutOfStock) {
-                $outOfStockProducts = array_filter($result, function ($row) {
-                    return $row['qte_stock'] <= $row['qte_min'];
-                });
-                if (!empty($outOfStockProducts)) {
-                    foreach ($outOfStockProducts as $row) {
-                        ?>
-                        <div class="product category-<?= $row['fk_idCat'] ?>">
-                            <img src="data:image/jpg;base64,<?= base64_encode($row['image']) ?>" alt="Product Image" width="300"
-                                height="300">
-                            <p>Libelle:
-                                <?= $row['libelle'] ?>
-                            </p>
-                            <p>Reference:
-                                <?= $row['ref_prod'] ?>
-                            </p>
-                            <p>Prix unitaire:
-                                <?= $row['pru'] ?>
-                            </p>
-                            <p>Quantité stock:
-                                <?= $row['qte_stock'] ?>
-                            </p>
-                        </div>
-
+            <!-- Sidebar -->
+            <nav class="col-md-2 d-none d-md-block bg-dark sidebar">
+                <div class="sidebar-sticky">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link " href="#" data-view="all_products">
+                                <i class="fas fa-home"></i> Tout les produits
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" data-view="out_of_stock">
+                                <i class="fas fa-shopping-cart"></i> Rupture de stock
+                            </a>
+                        </li>
                         <?php
-                    }
-                } else {
-                    ?>
-                    <p>No out-of-stock products found</p>
-                    <?php
-                }
-            } else {
-                if ($result) {
-                    foreach ($result as $row) {
+                        // Fetch categories from the database
+                        $sql = "SELECT idCat,nomCat FROM categorie";
+                        $result = $pdo->query($sql);
+
+                        // Display categories in the sidebar
+                        if ($result->rowCount() > 0) {
+                            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                                echo '<li class="nav-item">
+                                    <a class="nav-link" href="#" data-view="' . $row["nomCat"] . '">
+                                        <i class="fas fa-users"></i> ' . $row["nomCat"] . '
+                                    </a>
+                                </li>';
+                            }
+                        }
                         ?>
-                        <div class="product category-<?= $row['fk_idCat'] ?>">
-                            <img src="data:image/jpg;base64,<?= base64_encode($row['image']) ?>" alt="Product Image" width="300"
-                                height="300">
-                            <p>Libelle:
-                                <?= $row['libelle'] ?>
-                            </p>
-                            <p>Reference:
-                                <?= $row['ref_prod'] ?>
-                            </p>
-                            <p>Prix unitaire:
-                                <?= $row['pru'] ?>
-                            </p>
-                            <p>Quantité minimale:
-                                <?= $row['qte_min'] ?>
-                            </p>
-                            <p>Quantité stock:
-                                <?= $row['qte_stock'] ?>
-                            </p>
-                        </div>
+                        <!-- Add more sidebar items as needed -->
+                    </ul>
+                </div>
+            </nav>
 
-                        <?php
-                    }
-                } else {
-                    ?>
-                    <p>no record found</p>
-                    <?php
-                }
+            <!-- Main Content -->
+            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4" id="main-content">
 
-            }
+            </main>
 
-
-
-
-
-            ?>
         </div>
-    </section>
+    </div>
 
-
-
-
-
-
-    <footer style="color:white;background-color:rgb(3, 56, 26);text-align: center;
-  padding: 10px;
-  bottom: 0;
-  width: 100%;">
-        <p>copyright ElectroNAcer 2023</p>
+    <footer class="footer mt-auto py-3">
+        <div class="container text-center">
+            <p>&copy; 2023 ElectroNacer</p>
+            <p><a href="#">Privacy Policy</a> | <a href="#">Terms of Service</a></p>
+        </div>
     </footer>
 
 
 
+
+
+    <!-- Font Awesome for icons -->
+    <script src="https://kit.fontawesome.com/your-fontawesome-kit-id.js" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS and dependencies -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+
+    <!-- Your custom JavaScript -->
     <script src="assets/js/main.js"></script>
 </body>
 
